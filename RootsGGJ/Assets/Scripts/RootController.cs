8b7cb8d9caa13e137f4ea6e34602e254fcb979c1
DoctorRon64 <68813896+DoctorRon64@users.noreply.github.com>
2023-02-04 01:20:00 +0100

    update shit

--- RootsGGJ/Assets/Scripts/RootController.cs
+++ RootsGGJ/Assets/Scripts/RootController.cs
@@ -6,43 +6,44 @@ using UnityEngine;
 public class RootController : MonoBehaviour
 {
     public float MoveSpeed;
-    public float ScreenRes;
-    private float ySpeed;
-    public Rigidbody2D rb2d;
     Vector3 mousePosition;
-    public bool GoingUnder;
+    public TrailRenderer trailRenderer;
 
-    void Awake()
+    private void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        LookAndFollow();
+        trailRenderer = GetComponent<TrailRenderer>();
     }
 
     void FixedUpdate()
     {
-        rb2d.AddForce(transform.up);
+        LookAndFollow();
+        Color myColor = Color.white;
+       
+        trailRenderer.material.color = myColor;
+
+        myColor = Color.red;
     }
 
     void LookAndFollow()
     {
-        mousePosition.z = 0;
-
-        while(!GoingUnder)
-        {
-            ySpeed += ScreenRes;
-        }
-        Vector3 movement = new Vector3(mousePosition.x, ySpeed, 0);
-        transform.position = Vector3.Lerp(transform.position, movement, Time.deltaTime * MoveSpeed);
         
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * MoveSpeed * horizontalInput);
+        transform.Translate(Vector2.down * MoveSpeed);
+    }
 
-        Vector3 direction = (mousePosition - transform.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Border"))
+        {
+            RootDied();
+        }
+    }
+
+    void RootDied()
+    {
+        Debug.Log("obama gaming");
+        Vector2 RespawnLocation = new Vector2(0, 0);
+        transform.position = RespawnLocation;
     }
 }
---
+++ RootsGGJ/Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 0;
+        Vector3 direction = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
