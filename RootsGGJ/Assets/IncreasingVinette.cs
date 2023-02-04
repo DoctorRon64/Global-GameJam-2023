d3faa38cf829c4c25ca20cd6a2762dc25c3ebbae
DoctorRon64 <68813896+DoctorRon64@users.noreply.github.com>
2023-02-04 13:52:34 +0100

    update on enemy and postprocess

---
+++ RootsGGJ/Assets/IncreasingVinette.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+
+public class IncreasingVinette : MonoBehaviour
+{
+    private Transform player;
+    public PostProcessVolume volume;
+    public GameObject target;
+
+    void Awake()
+    {
+        player = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IfLower();
+    }
+
+    void IfLower()
+    {  
+        var volume = target.GetComponent<PostProcessVolume>();
+        if (volume.profile.TryGetSettings<Vignette>(out var Vignette))
+        {
+            Vignette.intensity.value = 2f;
+        }
+    }
+}
---
+++ RootsGGJ/Assets/Scripts/Enemy.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public enum StateEnum { Idle, Patrol, Attack }
+    public StateEnum currentState;
+    public float ViewDistance = 5;
+	[SerializeField] private float cooldownTimer;
+	public float CooldownStartTime;
+
+	[Header("Reference Object")]
+	public GameObject Player;
+
+    private Vector3 target;
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        target = new Vector3(Random.Range(-30, 30), Random.Range(-30, 30), 0);
+    }
+
+	private void Update()
+	{
+		
+        CheckState();
+	}
+
+	private void CheckState()
+	{
+        switch (currentState)
+		{
+			case StateEnum.Idle: IdleBehaviour(); break;
+			case StateEnum.Patrol: PatrolBehaviour(); break;
+			case StateEnum.Attack: AttackBehaviour(); break;
+   		}
+	}
+
+	private void IdleBehaviour()
+	{
+
+		if (Vector3.Distance(transform.position, Player.transform.position) < ViewDistance)
+		{
+			currentState = StateEnum.Attack;
+		}
+
+		cooldownTimer = CooldownStartTime;
+		cooldownTimer -= 1000 * Time.deltaTime;
+		if (cooldownTimer < 0)
+		{
+            target = new Vector3(Random.Range(-30, 30), Random.Range(-30, 30), 0);
+            currentState = StateEnum.Patrol;
+		}
+
+	}
+
+	private void PatrolBehaviour()
+	{
+		cooldownTimer = CooldownStartTime;
+		agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
+		Vector3 AgentDesti = agent.destination;
+
+		if (Vector3.Distance(transform.position, AgentDesti) < ViewDistance) {
+			currentState = StateEnum.Idle;
+		}
+
+		if (Vector3.Distance(transform.position, Player.transform.position) < ViewDistance)
+		{
+			currentState = StateEnum.Attack;
+		}
+	}
+
+	private void AttackBehaviour()
+	{
+		agent.SetDestination(Player.transform.position);
+        agent.transform.rotation = Quaternion.RotateTowards(transform.rotation, Player.transform.rotation, 360);
+
+        if (Vector3.Distance(transform.position, Player.transform.position) > ViewDistance*2)
+		{
+			currentState = StateEnum.Patrol;
+		}
+	}
+
+	private void OnTriggerEnter(Collider collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
